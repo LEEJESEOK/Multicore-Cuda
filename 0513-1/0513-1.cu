274213fe0f8b4846500ab6dc7e01b4ea37933280
LEEJESEOK <junelle@koreatech.ac.kr>
2018-06-15 14:37:59 +0000

    replace NUM_THREAD_IN_BLOCK

--- 0513-1/0513-1.cu
+++ 0513-1/0513-1.cu
@@ -12,7 +12,7 @@ __global__ void matAdd(int *a, int *b, int *c)
 {
 	const int NUM_THREAD_IN_BLOCK = blockDim.x * blockDim.y * blockDim.z;
 
- 	int bID = blockIdx.z * (gridDim.y * gridDim.x * NUM_THREAD_IN_BLOCK) + blockIdx.y * (gridDim.x * NUM_THREAD_IN_BLOCK) + (blockIdx.x * (blockDim.x * blockDim.y * blockDim.z));
+ 	int bID = blockIdx.z * (gridDim.y * gridDim.x * NUM_THREAD_IN_BLOCK) + blockIdx.y * (gridDim.x * NUM_THREAD_IN_BLOCK) + (blockIdx.x * NUM_THREAD_IN_BLOCK);
 	int tID = bID + ((blockDim.y * blockDim.x) * threadIdx.z) + (blockDim.x * threadIdx.y) + threadIdx.x;
 	
 	c[tID] = a[tID] + b[tID];
